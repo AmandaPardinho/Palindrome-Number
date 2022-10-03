db752892fdb631677482262996e2635fee0c5252
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-10-02 22:41:51 -0300

    Add variables, class Main and main method

--- palindrome.java
+++ palindrome.java
@@ -2,15 +2,27 @@ package palindrome;
 
 import java.util.*;
 
-switch (number){
-	case 1:
-		ab = x = ba;
-		break;
-	case 2:
-		abc = x = cba;
-		break;
-	case 3:
-		abcd = x = dcba;
-		break;
-	default: "Number not allowed";
+public class Main{
+
+	public static void main(String[] args) {
+
+		Integer x;
+		boolean isPalindrome;
+
+		switch (number){
+			case 1:
+				ab = x = ba;
+				isPalindrome = true;
+				break;
+			case 2:
+				abc = x = cba;
+				break;
+			case 3:
+				abcd = x = dcba;
+				break;
+			default: "Number not allowed";
+		}
+		
+	}
+
 }
