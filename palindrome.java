d0e7da3579be607bdb139fcf91ad6054f0c09bc4
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-10-02 22:22:29 -0300

    Starting the palindrome problem

---
+++ palindrome.java
@@ -0,0 +1,16 @@
+package palindrome;
+
+import java.util.*;
+
+switch (number){
+	case 1:
+		ab = x = ba;
+		break;
+	case 2:
+		abc = x = cba;
+		break;
+	case 3:
+		abcd = x = dcba;
+		break;
+	default: "Number not allowed";
+}
