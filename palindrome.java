5e1c36df9119456d7bf18cfb7b7c3c4c4216fdd3
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-10-03 21:52:52 -0300

    Rewrite switch/case

--- palindrome.java
+++ palindrome.java
@@ -1,6 +1,7 @@
 package palindrome;
 
 import java.util.*;
+import java.lang.Math;
 
 public class Main{
 
@@ -11,14 +12,17 @@ public class Main{
 
 		switch (number){
 			case 1:
-				ab = x = ba;
-				isPalindrome = true;
+				a = x = a;
 				break;
 			case 2:
+				ab = x = ba;
+				isPalindrome = true;
+			case 3:
 				abc = x = cba;
 				break;
-			case 3:
+			case 4:
 				abcd = x = dcba;
+				break;
 				break;
 			default: "Number not allowed";
 		}
